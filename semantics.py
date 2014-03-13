1752d4999866e4cb6fcb4bdfed7e4820e9b5e86e
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2014-03-13 10:19:41 -0400

    Adding Queue class
    The idea is that whebever run is invoked, a  class queue
    is created and job names specified in run are enqueued there.
    The jobs dedicated to run are previously constructed, thus
    we only need to add their dependencies, before enqueue them.

--- semantics.py
+++ semantics.py
@@ -2,26 +2,27 @@
 Implementation of semantic actions
 '''
 
-from Queue import PriorityQueue
 from subprocess import Popen, wait
 
 
-Queue =  dict()
+# This is a dictionary keeping all
+# Jobs that have been constructed.
 Jobs = dict()
+
 
 class Job():
     '''Constructor of class should be supplied with job name and
     respective script name
     '''
     def __init__(self, name, script, dependencies=None, workers=1):
-        self.script = script
         self.dependencies = dependencies
-        self.errno = 0
+        self.workers = workers
+        self.script = script
         self.stderr = None
         self.stdout = None
-        self.workers = workers
-        self.name =  name
+        self.errno = None  # errno is None since job has not run
         Jobs[name] = self
+        self.name = name
 
     def perror(self):
         return self.errno, self.stderr
@@ -34,35 +35,51 @@ class Job():
         depends on.
         '''
         for name in depends_on:
-            self.dependencies.append(name) 
-    
+            self.dependencies.append(name)
+
     def run(self):
         '''this should do the remote execution of scripts'''
-       s = subprocess.Popen(self.script, stdout=subprocess.PIPE)
-       streamdata = s.communicate()
-       self.output = streamdata[0]
-       self.stderr = streamdata[1]
-       self.errno = s.returncode
+        s = subprocess.Popen(self.script, stdout=subprocess.PIPE)
+        streamdata = s.communicate()
+        self.output = streamdata[0]
+        self.stderr = streamdata[1]
+        self.errno = s.returncode
 
     def can_run():
-        '''check if dependencies are fullfilled'''
+        '''check if dependencies are fullfilled.
+        Current instrance can run only if all jobs from
+        its dependency list have successed ( errno is zero )
+        '''
         for job in self.dependencies:
-            if Jobs[job].perror() != 0, _ :
+            if Jobs[job].perror() != (0, _):
                 return False
         return True
 
 
-def run():
-    '''construct priority queue'''
+class JobQueue:
+    '''Construct job queue as a list whose first element
+    are job names of jobs with zero dependencies,
+    second are job names of jobs with one dependency, etc.
+    '''
+    def __init__(self,  MaxDependencies=10):
+        self.Q = []
+        for _ in range(MaxDependencies):
+            self.Q.append([])
+
+    def enqueue(self, job, dependencies):
+        self.Q[dependencies] = job
+
+
+def run(Queue):
+    '''Try to run job from queue'''
+    # suppose queue is constructed.
     #
-    # suppose priority queue is constructed...
-    #
-    # e.g. Q = [[a,b],[c]], means that jobs with one 
-    # dependency i.e., Q[0] = [a,b] should run before 
-    # jobs with two dependencies, i.e., Q[1] = [c]   
+    # e.g. Q = [[a,b],[c]], means that jobs with one
+    # dependency i.e., Q[0] = [a,b] should run before
+    # jobs with two dependencies, i.e., Q[1] = [c]
     #
     for i in range(len(Queue)):
-        while not Queue[i]
+        while not Queue[i]:
             for job in Queue[i]:
                 if job.can_run():
                     Queue[i].remove(job)
