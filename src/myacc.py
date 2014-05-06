fbbbb6bbf23999b13bae656b2100e8ae9635a498
matlecu <mathias.lecuyer@gmail.com>
2014-05-06 18:16:27 -0400

    refactor

--- src/myacc.py
+++ src/myacc.py
@@ -15,22 +15,30 @@ precedence = (
 # LII is a comma separated list of Expressions
 def p_func_call(p):
     'E : ID LP LII RP'
-    p[0] = AST_obj(eval_func(p[1], p[3].value), Node(p[1], p[3].node))
-
+    val = eval_func(p[1], p[3].node.value)
+    _type = 'list'
+    node = Node(p[1], [p[3].node], _type, val)
+    p[0] = AST_obj(node)
 
 # assign a variable:
 # - put the name in the sym_table
 # - the expresion gets the value for 1 liners
 def p_assign(p):
     'E : ID ASSIGN E'
-    sym_table[p[1]] = p[3]
-    p[0] = AST_obj(p[3].value, Node('=', p[3].node))
+    val = p[3].node.value
+    _type = p[3].node._type
+    node = Node('=', [p[3].node], _type, val)
+    sym_table[p[1]] = node
+    p[0] = AST_obj(node)
 
 
 # strings for Job names
 def p_e_str(p):
     'E : STR'
-    p[0] = AST_obj(p[1], Node('str', leaf=p[1], n_type='String'))
+    val = p[1]
+    _type = 'sting'
+    node = Node('str', [], _type, val, leaf=True)
+    p[0] = AST_obj(node)
 
 
 # do we need that later?
@@ -44,40 +52,51 @@ def p_e_str(p):
 # arguments of a function or inside of a list for later
 def p_list_inside_grow(p):
     'LII : LII COMMA E'
-    p[0] = AST_obj(p[1].value + [p[3].value],  \
-            Node('list-concat', [p[1].node, p[3].node], n_type='List'))
+    val = p[1].node.value + [p[3].node.value]
+    _type = None
+    node = Node('list-concat', [p[1].node, p[3].node], _type, val)
+    p[0] = AST_obj(node)
 
 
 def p_list_inside_orig(p):
     'LII : E'
-    p[0] = AST_obj([p[1].value],  \
-            Node('list-orig', p[1].node, n_type='List'))
+    val = [p[1].node.value]
+    _type = p[1].node._type
+    node = Node('list-orig', [p[1].node], _type, val, leaf=True)
+    p[0] = AST_obj(node)
 
 
 # <->
 def p_e_nodep(p):
     'E : E NODEP E'
-    p[0] = AST_obj(nodep(p[1].value, p[3].value),  \
-            Node('<->', [p[1].node, p[3].node], n_type='List'))
+    val = nodep(p[1].node.value, p[3].node.value)
+    _type = 'list'
+    node = Node('<->', [p[1].node, p[3].node], _type, val)
+    p[0] = AST_obj(node)
 
 
 # ->
 def p_e_dep(p):
     'E : E DEP E'
-    p[0] = AST_obj(dep(p[1].value, p[3].value),  \
-            Node('->', [p[1].node, p[3]].node, n_type='List'))
+    val = dep(p[1].node.value, p[3].node.value)
+    _type = 'list'
+    node = Node('->', [p[1].node, p[3].node], _type, val)
+    p[0] = AST_obj(node)
 
 
 # ()
 def p_e_parenthesize(p):
     'E : LP E RP'
-    p[0] = AST_obj(p[2].value, Node('()', [p[2].node]))
+    p[0] = p[2]
 
 
 # that's a variable: fetch it in the symbol table
 def p_e_id(p):
     'E : ID'
-    p[0] = AST_obj(sym_table[p[1]], Node('id', leaf=p[1]))
+    val = sym_table[p[1]].value
+    _type = sym_table[p[1]]._type
+    node = Node('id', [], _type, val, leaf=True)
+    p[0] = AST_obj(node)
 
 # Error rule for syntax errors
 def p_error(p):
@@ -94,7 +113,7 @@ def eval_func(name, args):
     if name == "Job":
         return hj.Job(args[0], args[0])
     if name == "run":
-        hj.run([j.value for j in args])
+        hj.run([j for j in args])
         return args  # useful to reuse in a one liner
 
 
@@ -119,20 +138,21 @@ def dep(jobs, depend_on_jobs):
 
 # AST node structure
 class Node:
-    def __init__(self, operation, children=None, leaf=None, n_type=None):
-         self.n_type = n_type
-         self.operation = operation
-         if children:
-              self.children = children
-         else:
-              self.children = []
-         self.leaf = leaf
+    def __init__(self, operation, children=None,  \
+                       _type=None, value=None, leaf=False):
+        self._type = _type
+        self.operation = operation
+        if children:
+            self.children = children
+        else:
+            self.children = []
+        self.leaf = leaf
+        self.value = value
 
 # we add one layer of abstraction to be able to get values and syblings
 # on top of node
 class AST_obj:
-    def __init__(self, value=None, node=None, syblings=None):
-        self.value = value
+    def __init__(self, node=None, value=None, syblings=None):
         self.node = node
         self.syblings = syblings
 
@@ -160,9 +180,3 @@ if __name__ == '__main__':
         f.close()
     else:
         print "Usage: python myacc.py <file_name>"
-#       s = raw_input('maestro> ')
-#   except EOFError:
-#       break
-#   if not s: continue
-#   result = parser.parse(s)
-#   print result
