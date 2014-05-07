1581c9b752bacfa8d8b5705923ee18c788c89b3d
matlecu <mathias.lecuyer@gmail.com>
2014-05-07 16:44:21 -0400

    striped strings

--- src/myacc.py
+++ src/myacc.py
@@ -63,7 +63,7 @@ def p_assign(p):
 def p_e_str(p):
     'E : STR'
     _type = 'sting'
-    node = Node('str', [], _type, value=str(p[1]), leaf=True)
+    node = Node('str', [], _type, value=str(p[1][1:-1]), leaf=True)
     p[0] = AST_obj(node)
 
 def p_e_int(p):
