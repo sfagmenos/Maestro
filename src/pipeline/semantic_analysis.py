79ccace976640cabba8f8790b48a4349ff8fafc9
Georgios Koloventzos <g.koloventzos@gmail.com>
2014-05-07 13:21:20 -0400

    adding some semantic checks

--- src/pipeline/semantic_analysis.py
+++ src/pipeline/semantic_analysis.py
@@ -1,2 +1,12 @@
 def analyse(ast):
-    pass
+#    pass
+    if not ast.children:
+        return ast._type
+    for node in ast.children:
+        if node.operation == '<->' or node.operation == '->':
+            type1 = analyse(node[0])
+            type2 = analyse(node[1])
+            if type1 != type2 or type1 != "Job" or type2 != "Job":
+                return "Error"
+            else:
+                return type1
